a519d19f87505ccc685d6ff6fcb70a6eab127a0f
Mario Renaldy <6181901030@student.unpar.ac.id>
2021-10-28 01:11:39 +0700

    update 1.1 Add Poster

--- app/src/main/java/com/example/tubesp3b/EditDetailFragment.java
+++ app/src/main/java/com/example/tubesp3b/EditDetailFragment.java
@@ -1,24 +1,36 @@
 package com.example.tubesp3b;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentResultListener;
 
 import com.example.tubesp3b.databinding.FragmentEditDetailBinding;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class EditDetailFragment extends Fragment implements View.OnClickListener{
     private FragmentEditDetailBinding binding;
     private EditDetailPresenter presenter;
     private int moviePosition;
+    private ActivityResultLauncher<Intent> intentLauncher;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,6 +55,28 @@ public class EditDetailFragment extends Fragment implements View.OnClickListener
                 binding.etReview.setText(movie.getReview());
             }
         });
+
+        this.intentLauncher = this.registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode() == Activity.RESULT_OK){
+                            Intent reply = result.getData();
+                            Uri selectedImage = reply.getData();
+                            try (ParcelFileDescriptor pfd = getContext().getContentResolver().openFileDescriptor(selectedImage, "r")) {
+                                if (pfd != null) {
+                                    Bitmap bitmap = BitmapFactory.decodeFileDescriptor(pfd.getFileDescriptor());
+                                    binding.ivPoster.setImageBitmap(bitmap);
+                                }
+                            } catch (IOException ex) {
+
+                            }
+                        }
+                    }
+                });
+
+        binding.btnChooseImage.setOnClickListener(this);
         binding.btnSave.setOnClickListener(this);
         return view;
     }
@@ -53,17 +87,23 @@ public class EditDetailFragment extends Fragment implements View.OnClickListener
 
     @Override
     public void onClick(View view) {
-        String title = binding.etTitle.getText().toString();
-        String synopsis = binding.etSynopsis.getText().toString();
+        if(view == binding.btnChooseImage){
+            Intent chooseImageIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intentLauncher.launch(chooseImageIntent);
+        }
+        else {
+            String title = binding.etTitle.getText().toString();
+            String synopsis = binding.etSynopsis.getText().toString();
 
-        binding.ivPoster.buildDrawingCache();
-        Bitmap bm = binding.ivPoster.getDrawingCache();
-        String poster = getEncoded64ImageStringFromBitmap(bm);
+            binding.ivPoster.buildDrawingCache();
+            Bitmap bm = binding.ivPoster.getDrawingCache();
+            String poster = getEncoded64ImageStringFromBitmap(bm);
 
-        String status = binding.etStatus.getText().toString();
-        String review = binding.etReview.getText().toString();
-        Movie editedMovie = new Movie(title, synopsis, poster, status, review);
-        presenter.saveDetails(editedMovie, moviePosition);
+            String status = binding.etStatus.getText().toString();
+            String review = binding.etReview.getText().toString();
+            Movie editedMovie = new Movie(title, synopsis, poster, status, review);
+            presenter.saveDetails(editedMovie, moviePosition);
+        }
     }
     public String getEncoded64ImageStringFromBitmap(Bitmap bitmap) {
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
--- app/src/main/java/com/example/tubesp3b/FragmentTambahFilm.java
+++ app/src/main/java/com/example/tubesp3b/FragmentTambahFilm.java
@@ -1,9 +1,14 @@
 package com.example.tubesp3b;
 
 import android.content.SharedPreferences;
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,11 +17,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 
 import com.example.tubesp3b.databinding.TambahFilmBinding;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class FragmentTambahFilm extends Fragment implements View.OnClickListener{
     TambahFilmBinding binding;
@@ -24,6 +34,7 @@ public class FragmentTambahFilm extends Fragment implements View.OnClickListener
     private EditText et_judulFilm;
     private EditText et_SinopsisFilm;
     private ImageView poster;
+    private ActivityResultLauncher<Intent> intentLauncher;
     private PenyimpananNilai pencatat;
 
     @Override
@@ -32,10 +43,35 @@ public class FragmentTambahFilm extends Fragment implements View.OnClickListener
         this.binding = TambahFilmBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
         this.btn_tambahFilm = this.binding.btnTambahfilm;
+        this.et_judulFilm = this.binding.etJudul;
+        this.et_SinopsisFilm = this.binding.etSinopsis;
+        this.poster = this.binding.gambar1;
+
         this.btn_tambahFilm.setOnClickListener(this);
+        binding.uploadPoster.setOnClickListener(this);
 
         this.pencatat = new PenyimpananNilai(this.binding.etJudul.getContext());
         this.pencatat = new PenyimpananNilai(this.binding.etSinopsis.getContext());
+
+        this.intentLauncher = this.registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode() == Activity.RESULT_OK){
+                            Intent reply = result.getData();
+                            Uri selectedImage = reply.getData();
+                            try (ParcelFileDescriptor pfd = getContext().getContentResolver().openFileDescriptor(selectedImage, "r")) {
+                                if (pfd != null) {
+                                    Bitmap bitmap = BitmapFactory.decodeFileDescriptor(pfd.getFileDescriptor());
+                                    binding.gambar1.setImageBitmap(bitmap);
+                                }
+                            } catch (IOException ex) {
+
+                            }
+                        }
+                    }
+                });
 
         return view;
     }
@@ -55,6 +91,10 @@ public class FragmentTambahFilm extends Fragment implements View.OnClickListener
            // this.storeInternal(this.binding.etJudul.getEditableText().toString() + this.binding.etSinopsis.getEditableText().toString());
 
         }
+        else{
+            Intent chooseImageIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intentLauncher.launch(chooseImageIntent);
+        }
     }
     public static FragmentTambahFilm newInstance(){
         FragmentTambahFilm fragment = new FragmentTambahFilm();
@@ -67,5 +107,4 @@ public class FragmentTambahFilm extends Fragment implements View.OnClickListener
         String imgString = Base64.encodeToString(byteFormat, Base64.NO_WRAP);
         return imgString;
     }
-
 }
